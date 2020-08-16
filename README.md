c04a84e76d7e0ca208660f7388327b2e378961f0
Naoki Oketani <okepy.naoki@gmail.com>
2020-08-16 16:37:27 +0900

    v1.18

--- README.md
+++ README.md
@@ -1,10 +1,16 @@
+## install
+
+```
+go install .
+```
+
 ## Sample
 
 ```
 export ISSUE_CREATOR_TOKEN=xxxxxxxx
-export IC_OLD_REVISION=upstream/release-1.15
-export IC_NEW_REVISION=upstream/release-1.17
-export IC_FILEPATH=docs/concepts/workloads/pods/pod-overview.md
+export IC_OLD_REVISION=upstream/release-1.17
+export IC_NEW_REVISION=fb6364d
+export IC_FILEPATH=docs/concepts/workloads/pods/pod-lifecycle.md
 
 git diff $IC_OLD_REVISION $IC_NEW_REVISION content/en/$IC_FILEPATH | issue-creator
 https://github.com/kubernetes/website/issues/new?title=...&body=...
--- issue/create.go
+++ issue/create.go
@@ -13,7 +13,7 @@ func CreateBody(path string, gistURL string) string {
 <!-- See https://kubernetes.io/docs/contribute/start/ for guidance on writing an actionable issue description. -->
 
 **What would you like to be added**
-Update %s to follow v1.17 of the original (English) text.
+Update %s to follow v1.18 of the original (English) text.
 
 **Why is this needed**
 content/ja/%s is outdated.
@@ -23,20 +23,19 @@ content/ja/%s is outdated.
 /good-first-issue
 
 File to update
-https://github.com/kubernetes/website/blob/dev-1.17-ja.3/content/ja/%s
+https://github.com/kubernetes/website/blob/dev-1.18-ja.1/content/ja/%s
 
 Original
-https://github.com/kubernetes/website/blob/release-1.17/content/en/%s
+https://github.com/kubernetes/website/blob/fb6364d/content/en/%s
 
-diff between translated and v1.17
+diff between translated and v1.18
 %s
 
 **Note**
-Currently, we use **dev-1.17-ja.3** branch. Please open a PR targeting the branch.
+Currently, we use **dev-1.18-ja.1** branch. Please open a PR targeting the branch.
 
 ref
-[How To Contribute](https://kubernetes-docs-ja.kibe.la/shared/entries/c5878aa5-ad1f-4f29-a5bb-25853cbc14ec)
-[翻訳スタイルガイド](https://kubernetes-docs-ja.kibe.la/shared/entries/5efe4fa7-d2a1-4a1d-8bc3-ce7ccdc064a6)
+https://kubernetes.io/ja/docs/contribute/localization/
 
 If you have a question, feel free to ask us at slack.k8s.io #kubernetes-docs-ja channel.`,
 		path, path, path, path, gistURL,
@@ -46,7 +45,7 @@ If you have a question, feel free to ask us at slack.k8s.io #kubernetes-docs-ja 
 // Create creates a new issue and return the URL
 func Create(path string, gistURL string) string {
 	return fmt.Sprintf("https://github.com/kubernetes/website/issues/new?title=%s&body=%s",
-		url.QueryEscape(fmt.Sprintf("ja: Make %s follow v1.17 of the original text", path)),
+		url.QueryEscape(fmt.Sprintf("ja: Make %s follow v1.18 of the original text", path)),
 		url.QueryEscape(CreateBody(path, gistURL)),
 	)
 }
